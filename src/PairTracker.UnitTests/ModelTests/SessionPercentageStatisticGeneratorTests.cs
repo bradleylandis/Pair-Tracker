ae3a194a06478c61f5a72533dbce9e235ddd7069
Bradley T. Landis <bradleylandis@bradleylandis.com>
2011-05-11 13:52:12 -0400

    changed so that Neither does not count toward percentages of session

--- src/PairTracker.UnitTests/ModelTests/SessionPercentageStatisticGeneratorTests.cs
+++ src/PairTracker.UnitTests/ModelTests/SessionPercentageStatisticGeneratorTests.cs
@@ -43,6 +43,25 @@ namespace PairTracker.UnitTests.ModelTests
         }
 
         [Test]
+        public void SessionWith3EqualIntervalsWith1BeingNeitherResultsIn2ProgrammersWith50PercentEach()
+        {
+            var programmerJoe = new Programmer("Joe");
+            var programmerBob = new Programmer("Bob");
+            var intervals = new List<Interval>();
+            intervals.Add(new TestInterval(new DateTime(2000, 1, 1, 0, 0, 0), new TimeSpan(0, 0, 5), programmerJoe));
+            intervals.Add(new TestInterval(new DateTime(2000, 1, 1, 0, 0, 5), new TimeSpan(0, 0, 5),Programmer.Neither));
+            intervals.Add(new TestInterval(new DateTime(2000, 1, 1, 0, 0, 10), new TimeSpan(0, 0, 5), programmerBob));
+            var session = new TestSession(programmerJoe, programmerBob, intervals);
+
+            var statGenerator = new SessionPercentageStatisticGenerator();
+            var results = statGenerator.Generate(session);
+
+            Assert.That(results.Count, Is.EqualTo(2));
+            Assert.That(results[programmerJoe], Is.EqualTo(50));
+            Assert.That(results[programmerBob], Is.EqualTo(50));
+        }
+
+        [Test]
         public void SessionWith2IntervalsForSameProgrammerResultsIn1ProgrammersWith100Percent()
         {
             var programmerJoe = new Programmer("Joe");
--- src/PairTracker/Model/SessionPercentageStatisticGenerator.cs
+++ src/PairTracker/Model/SessionPercentageStatisticGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace PairTracker.Model
 {
@@ -11,24 +12,31 @@ namespace PairTracker.Model
             return GetPercentageOfSessionByProgrammer(totalSecondsInSessionByProgrammer, session);
         }
 
-        private IDictionary<Programmer, double> GetTotalSecondsByProgrammer(Session session)
+        private IDictionary<Programmer, TimeSpan> GetTotalSecondsByProgrammer(Session session)
         {
-            var totalSecondsInSessionByProgrammer = new Dictionary<Programmer, double>();
+            var totalSecondsInSessionByProgrammer = new Dictionary<Programmer, TimeSpan>();
             foreach (var interval in session.Intervals)
             {
                 if (totalSecondsInSessionByProgrammer.ContainsKey(interval.Programmer))
-                    totalSecondsInSessionByProgrammer[interval.Programmer] += interval.Length.TotalSeconds;
+                    totalSecondsInSessionByProgrammer[interval.Programmer] += interval.Length;
                 else
-                    totalSecondsInSessionByProgrammer.Add(interval.Programmer, interval.Length.TotalSeconds);
+                    totalSecondsInSessionByProgrammer.Add(interval.Programmer, interval.Length);
             }
             return totalSecondsInSessionByProgrammer;
         }
 
-        private IDictionary<Programmer, int> GetPercentageOfSessionByProgrammer(IDictionary<Programmer, double> totalSecondsInSessionByProgrammer, Session session)
+        private IDictionary<Programmer, int> GetPercentageOfSessionByProgrammer(IDictionary<Programmer, TimeSpan> totalSecondsInSessionByProgrammer, Session session)
         {
             var percentageOfSessionByProgrammer = new Dictionary<Programmer, int>();
+
+            //TODO: Clean this up
+            TimeSpan timeControlledByNeither = new TimeSpan();
+            totalSecondsInSessionByProgrammer.TryGetValue(Programmer.Neither, out timeControlledByNeither);
+
+            var totalSecondsControlledByAProgrammer = session.Length - timeControlledByNeither;
             foreach (var item in totalSecondsInSessionByProgrammer)
-                percentageOfSessionByProgrammer.Add(item.Key, (int)((double)100 * (item.Value / session.Length.TotalSeconds)));
+                if(item.Key != Programmer.Neither)
+                    percentageOfSessionByProgrammer.Add(item.Key, (int)((double)100 * (item.Value.TotalSeconds / totalSecondsControlledByAProgrammer.TotalSeconds)));
 
             return percentageOfSessionByProgrammer;
         }
