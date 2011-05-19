3f166c4fbfbfd7ddb4d6826a6300d2b7c5ec3adf
Bradley T. Landis <bradleylandis@bradleylandis.com>
2011-05-18 23:43:02 -0400

    Fix issue where it was counting different instances of same programmer

--- src/PairTracker/Model/Programmer.cs
+++ src/PairTracker/Model/Programmer.cs
@@ -14,5 +14,19 @@
         {
             return this.Name;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) 
+                return false;
+
+          var that = (Programmer)obj;
+          return this.Name ==that.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
     }
 }
