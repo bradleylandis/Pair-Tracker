44c7644f2faf63e10f65b647ae8961eb3484c27b
Bradley T. Landis <bradleylandis@bradleylandis.com>
2011-05-23 13:06:28 -0400

    clean up a little bit

--- src/PairTracker.UnitTests/TestDoubles/TestSession.cs
+++ src/PairTracker.UnitTests/TestDoubles/TestSession.cs
@@ -5,8 +5,7 @@ namespace PairTracker.UnitTests.TestDoubles
 {
     public class TestSession : PairingSessionImpl
     {
-        public TestSession(Programmer programmer1, Programmer programmer2, IList<Interval> intervals)
-            : base(null)
+        public TestSession(Programmer programmer1, Programmer programmer2, IList<Interval> intervals) : base(null)
         {
             this.Programmer1 = programmer1;
             this.Programmer2 = programmer2;
--- src/PairTracker/Model/SessionPercentageStatisticGenerator.cs
+++ src/PairTracker/Model/SessionPercentageStatisticGenerator.cs
@@ -7,21 +7,20 @@ namespace PairTracker.Model
     {
         public IDictionary<Programmer, Statistic> Generate(PairingSession session)
         {
-            var totalSecondsInSessionByProgrammer = GetTotalTimeByProgrammer(session);
+            var totalTimeInSessionByProgrammer = GetTotalTimeByProgrammer(session);
 
-            return GetPercentageOfSessionByProgrammer(totalSecondsInSessionByProgrammer, session);
+            return GetPercentageOfSessionByProgrammer(totalTimeInSessionByProgrammer, session);
         }
 
         private IDictionary<Programmer, TimeSpan> GetTotalTimeByProgrammer(PairingSession session)
         {
             var totalTimeInSessionByProgrammer = new Dictionary<Programmer, TimeSpan>();
             foreach (var interval in session.Intervals)
-            {
                 if (totalTimeInSessionByProgrammer.ContainsKey(interval.Programmer))
                     totalTimeInSessionByProgrammer[interval.Programmer] += interval.Length;
                 else
                     totalTimeInSessionByProgrammer.Add(interval.Programmer, interval.Length);
-            }
+
             return totalTimeInSessionByProgrammer;
         }
 
@@ -33,16 +32,18 @@ namespace PairTracker.Model
             TimeSpan timeControlledByNeither = new TimeSpan();
             totalSecondsInSessionByProgrammer.TryGetValue(Programmer.Neither, out timeControlledByNeither);
 
-            var totalSecondsControlledByAProgrammer = session.Length - timeControlledByNeither;
+            var totalTimeControlledByAProgrammer = session.Length - timeControlledByNeither;
             foreach (var item in totalSecondsInSessionByProgrammer)
                 if (item.Key != Programmer.Neither)
-                {
-                    int percentage =  (int)Math.Round((double)100 * (item.Value.TotalSeconds / totalSecondsControlledByAProgrammer.TotalSeconds));
-                    Statistic statistic = new Statistic(percentage, item.Value);
-                    percentageOfSessionByProgrammer.Add(item.Key, statistic);
-                }
+                    percentageOfSessionByProgrammer.Add(item.Key, CalculateStatistic(item.Value, totalTimeControlledByAProgrammer));
 
             return percentageOfSessionByProgrammer;
+        }
+
+        private Statistic CalculateStatistic(TimeSpan intervalLegth, TimeSpan totalTimeControlledByAProgrammer)
+        {
+            int percentage = (int)Math.Round((double)100 * (intervalLegth.TotalSeconds / totalTimeControlledByAProgrammer.TotalSeconds));
+            return new Statistic(percentage, intervalLegth);
         }
     }
 }
